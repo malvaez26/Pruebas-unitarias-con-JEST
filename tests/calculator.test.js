1a6a0b832e53971cee9c8819675d0fb7a6015784
Malvaez26 <daniel.malvaezh@gmail.com>
2024-01-22 17:01:49 -0600

    Se resolvio el ejercicio calculator.test.js

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -2,23 +2,23 @@ const { default: expect } = require('expect');
 
 const calculator = require('../calculator');
 
-test('adds a + b to equal 3', () => {
-    expect(calc.add(1,2)).toBe(3);
+test('Sum a + b to equal 3', () => {
+    expect(calculator.sum(1,2)).toBe(3);
 })
 
-test('substracts a - b', () => {
-   //
+test('substracts a - b to equal 0', () => {
+   expect(calculator.substract(3,3)).toBe(0)
 })
 
-test('multiplies a * b', () => {
-    //
+test('multiplies a * b to equal 20', () => {
+    expect(calculator.multiply(5,4)).toBe(20)
 })
 
-test('divides a / b', () => {
-    // Consider decimals
+test('divides of numbers a / b', () => {
+    expect(calculator.divide(1,1)).toBe(1);
 })
 
 test('Raises to power a ^ b', () => {
-    //
+    expect(calculator.power(2,2)).toBe(4);
 })
 
