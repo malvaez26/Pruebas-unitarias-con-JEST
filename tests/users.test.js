952d482ceb914cba5fba923d5facc3fe50909fc1
Malvaez26 <daniel.malvaezh@gmail.com>
2024-01-22 16:20:52 -0600

    Se hicieron pruebas con el ejercicio users.test.js

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,6 +1,19 @@
 // You already have the methods imported
-const { default: expect } = require('expect');
-const {getUser, getUsers} = require('../users');
+const users = require('../data');
+const { getUsersById, getUsersByUsername, validatePassword } = require('../users');
+
+describe('User functions tests', () => {
+    it('Should get the user by its id', () => {
+        const user = getUsersById(1);
+        expect(user).toEqual({
+            id: 1,
+            username:'hpLover4',
+            email: 'sirious90@gmail.com',
+            password: 'hArrydotCom'
+        })
+    })
+})
+
 
 
 // Make a test to verify if the method getUser(id) works correctly
--- users.js
+++ users.js
@@ -2,18 +2,33 @@
 // Pretend it's an API request
 const users = require('./data');
 
-// Fetches all users
-const getUsers = () => {
-    return users;
-}
+// // Fetches all users
+// const getUsers = () => {
+//     return users;
+// }
 
-// Filters users by specific ID
-const getUser = id => {
-    return users.find(user => user.id === id)
-}
+// // Filters users by specific ID
+// const getUser = id => {
+//     return users.find(user => user.id === id)
+// }
 
 
 // test
 // console.log(getUser(3));
 
-module.exports = {getUsers , getUser};
+// module.exports = {getUsers , getUser};
+
+function getUsersById(id) {
+    return users.find(user => user.id === id);
+}
+
+function getUsersByUsername(username) {
+    return user.find(user => user.username === username);
+}
+
+function validatePassword(userId, password) {
+    const user = getUsersById(userId);
+    return user ? user.password === password : false;
+}
+
+module.exports = { getUsersById, getUsersByUsername, validatePassword }
