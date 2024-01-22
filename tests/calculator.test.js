16cc5d9ddc6fd3bdd66383e9b918afa67ceec074
Malvaez26 <daniel.malvaezh@gmail.com>
2024-01-22 15:12:19 -0600

    Se empezo a tratar de resolver los ejercicios.

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -2,8 +2,8 @@ const { default: expect } = require('expect');
 
 const calculator = require('../calculator');
 
-test('adds a + b', () => {
-    //
+test('adds a + b to equal 3', () => {
+    expect(calc.add(1,2)).toBe(3);
 })
 
 test('substracts a - b', () => {
